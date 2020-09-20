a57004d78835a921fb8259dc0c254c6409a061ed
Daniel Muhra <daniel.muhra@caperwhite.com>
2020-09-21 00:04:58 +0200

    Fixed build issues

--- Core/Source/NSString+CSS.h
+++ Core/Source/NSString+CSS.h
@@ -6,7 +6,8 @@
 //  Copyright (c) 2012 Drobnik.com. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <DTCoreText/DTCompatibility.h>
 
 @class DTColor;
 
